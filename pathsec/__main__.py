c22ed6f4072ed44768cc10838572bb4f1364d0d7
Justin Furuness <jfuruness@gmail.com>
2024-03-28 01:14:35 -0400

    draft of hijack scenarios

--- pathsec/__main__.py
+++ pathsec/__main__.py
@@ -1,15 +1,22 @@
+from functools import partial
 import time
 
 from .sims import (
     run_origin_hijack_sim,
-    run_origin_spoofing_hijack_sim
+    run_origin_spoofing_hijack_sim,
+    run_shortest_path_export_all_hijack_sim,
 )
 
 
 def main():
     """Runs the defaults"""
 
-    sim_funcs = (run_origin_hijack_sim, run_origin_spoofing_hijack_sim)
+    sim_funcs = (
+        run_origin_hijack_sim,
+        run_origin_spoofing_hijack_sim,
+        run_shortest_path_export_all_hijack_sim,
+        partial(run_shortest_path_export_all_hijack_sim, num_attackers=10)
+    )
     for sim_func in sim_funcs:
         start = time.perf_counter()
         sim_func()
@@ -17,4 +24,6 @@ def main():
 
 
 if __name__ == "__main__":
+    start = time.perf_counter()
     main()
+    print(f"{time.perf_counter() - start}s for all sims")
--- pathsec/sims/__init__.py
+++ pathsec/sims/__init__.py
@@ -1,7 +1,11 @@
 from .run_origin_hijack_sim import run_origin_hijack_sim
 from .run_origin_spoofing_hijack_sim import run_origin_spoofing_hijack_sim
+from .run_shortest_path_export_all_hijack_sim import (
+    run_shortest_path_export_all_hijack_sim
+)
 
 __all__ = [
     "run_origin_hijack_sim",
     "run_origin_spoofing_hijack_sim",
+    "run_shortest_path_export_all_hijack_sim",
 ]
