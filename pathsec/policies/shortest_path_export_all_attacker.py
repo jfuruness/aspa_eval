9432b09fd7ddb299f08117e6bfbff96327747f25
Justin Furuness <jfuruness@gmail.com>
2024-04-04 04:55:56 -0400

    fixed the shortest path export all attacker

--- pathsec/policies/shortest_path_export_all_attacker.py
+++ pathsec/policies/shortest_path_export_all_attacker.py
@@ -16,7 +16,9 @@ class ShortestPathExportAllAttacker(BGP):
     we currently use the bulk of the shortest path export all functionality within
     the preprocess anns func, a bug in BGPy that we have to fix. So we harcode in our
     results to use the preprocess anns func, and then also use this policy, but we
-    don't assert this anywhere, so use outside of our results is not advised
+    don't assert this anywhere, so use outside of our results is not advised.
+
+    Also see comment below about using the timestamp... just use this policy here
     """
 
     def _policy_propagate(
@@ -29,7 +31,10 @@ class ShortestPathExportAllAttacker(BGP):
         """As defined in ASPA RFC section 12, use origin hijack for customers"""
 
         # This ann is the one we want to overwrite - the ann seeded at the attacker
-        if ann.seed_asn is not None and propagate_to == Relationships.CUSTOMER:
+        # NOTE: tried ann.seed_asn, but due to other funcs overwriting the ann.seed_asn,
+        # we must use the timestamp and just check for 1.
+        # Janky, but it works for these
+        if ann.timestamp == 1 and propagate_to == Relationships.CUSTOMERS:
             # Only need origin hijack when sending to customers
             new_ann = ann.copy({"as_path": (self.as_.asn, ann.origin)})
             self._process_outgoing_ann(neighbor, new_ann, propagate_to, send_rels)
