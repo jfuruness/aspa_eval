06356ccd006b95e4115f353ba931bab50e40ff85
Justin Furuness <jfuruness@gmail.com>
2024-04-18 19:45:44 -0400

    spea diagrams draft

--- pathsec/tests/engine_tests/engine_test_configs/diagrams/__init__.py
+++ pathsec/tests/engine_tests/engine_test_configs/diagrams/__init__.py
@@ -1,6 +1,7 @@
 from .rfc_config import rfc_config
 from .spea_aspa_config import spea_aspa_config
+from .spea_pathend_config import spea_pathend_config
 
-diagram_configs = (rfc_config, spea_aspa_config)
+diagram_configs = (rfc_config, spea_aspa_config, spea_pathend_config)
 
 __all__ = ["diagram_configs"]
---
+++ pathsec/tests/engine_tests/engine_test_configs/diagrams/spea_pathend_config.py
@@ -0,0 +1,38 @@
+from frozendict import frozendict
+
+from bgpy.as_graphs import ASGraphInfo, CustomerProviderLink as CPLink
+from bgpy.enums import ASNs
+from bgpy.simulation_engine import BGP, Pathend
+from bgpy.simulation_framework import (
+    preprocess_anns_funcs,
+    ScenarioConfig,
+    PrefixHijack
+)
+from bgpy.tests.engine_tests.utils import EngineTestConfig
+
+from .spea_aspa_config import as_graph_info
+
+
+spea_pathend_config = EngineTestConfig(
+    name="Shortest Path Export All Attack against Pathend",
+    desc="",
+    scenario_config=ScenarioConfig(
+        ScenarioCls=PrefixHijack,
+        preprocess_anns_func=preprocess_anns_funcs.shortest_path_export_all_hijack,
+        BasePolicyCls=BGP,
+        AdoptPolicyCls=Pathend,
+        override_victim_asns=frozenset({ASNs.VICTIM.value}),
+        override_attacker_asns=frozenset({ASNs.ATTACKER.value}),
+        override_non_default_asn_cls_dict=frozendict(
+            {
+                1: Pathend,
+                2: Pathend,
+                4: Pathend,
+                5: Pathend,
+                6: Pathend,
+                ASNs.VICTIM.value: Pathend,
+            }
+        ),
+    ),
+    as_graph_info=as_graph_info,
+)
