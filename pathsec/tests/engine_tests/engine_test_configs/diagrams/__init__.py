1d612e5f6bf01f5275209ca824a66ca7f745ea8b
Justin Furuness <jfuruness@gmail.com>
2024-04-18 19:19:03 -0400

    draft of scenario config for spea attack against aspa

--- pathsec/tests/engine_tests/engine_test_configs/diagrams/__init__.py
+++ pathsec/tests/engine_tests/engine_test_configs/diagrams/__init__.py
@@ -1,5 +1,6 @@
 from .rfc_config import rfc_config
+from .spea_aspa_config import spea_aspa_config
 
-diagram_configs = (rfc_config,)
+diagram_configs = (rfc_config, spea_aspa_config)
 
-__all__ = ["rfc_config"]
+__all__ = ["diagram_configs"]
---
+++ pathsec/tests/engine_tests/engine_test_configs/diagrams/spea_aspa_config.py
@@ -0,0 +1,60 @@
+from frozendict import frozendict
+
+from bgpy.as_graphs import ASGraphInfo, CustomerProviderLink as CPLink
+from bgpy.enums import ASNs
+from bgpy.simulation_engine import BGP, ASPA
+from bgpy.simulation_framework import preprocess_anns_funcs
+
+from bgpy.tests.engine_tests.utils import EngineTestConfig
+
+from pathsec.policies import ShortestPathExportAllAttacker
+from pathsec.attacker_mod_scenario import AttackerModScenario, AttackerModScenarioConfig
+
+
+as_graph_info = ASGraphInfo(
+    customer_provider_links=frozenset(
+        [
+            CPLink(provider_asn=4, customer_asn=ASNs.VICTIM.value),
+            CPLink(provider_asn=2, customer_asn=ASNs.VICTIM.value),
+            CPLink(provider_asn=ASNs.ATTACKER.value, customer_asn=1),
+            CPLink(provider_asn=3, customer_asn=1),
+            CPLink(provider_asn=3, customer_asn=2),
+            CPLink(provider_asn=4, customer_asn=ASNs.ATTACKER.value),
+            CPLink(provider_asn=5, customer_asn=ASNs.ATTACKER.value),
+            CPLink(provider_asn=6, customer_asn=ASNs.ATTACKER.value),
+            CPLink(provider_asn=6, customer_asn=3),
+        ]
+    ),
+    diagram_ranks=(
+        (4, 5, 6),
+        (ASNs.ATTACKER.value, 3),
+        (1, 2),
+        (ASNs.VICTIM.value,),
+    ),
+
+)
+
+
+spea_aspa_config = EngineTestConfig(
+    name="Shortest Path Export All Attack against ASPA",
+    desc="",
+    scenario_config=AttackerModScenarioConfig(
+        ScenarioCls=AttackerModScenario,
+        preprocess_anns_func=preprocess_anns_funcs.shortest_path_export_all_hijack,
+        BasePolicyCls=BGP,
+        AttackerBasePolicyCls=ShortestPathExportAllAttacker,
+        override_victim_asns=frozenset({ASNs.VICTIM.value}),
+        override_attacker_asns=frozenset({ASNs.ATTACKER.value}),
+        override_non_default_asn_cls_dict=frozendict(
+            {
+                1: ASPA,
+                2: ASPA,
+                4: ASPA,
+                5: ASPA,
+                6: ASPA,
+                ASNs.VICTIM.value: ASPA,
+            }
+        ),
+    ),
+    as_graph_info=as_graph_info,
+)
