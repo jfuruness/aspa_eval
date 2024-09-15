ed1c7a08768c1b7b36b9ef11a08539bcd286cd84
Justin Furuness <jfuruness@gmail.com>
2024-09-15 03:11:24 -0400

    still refactoring

--- pathsec/sims/run_sim.py
+++ pathsec/sims/run_sim.py
@@ -4,6 +4,7 @@ from bgpy.shared.enums import ASGroups
 from bgpy.simulation_engine import (
     ROV,
     BGPSec,
+    BGPiSec,
     PathEnd,
     ROVEdgeFilter
 )
@@ -26,8 +27,30 @@ def run_sim(opts: list[Opt], output_dir: Path | None = None) -> None:
             opts[0].ScenarioCls.__name__.lower() /
             opts[0].attacker_subcategory_attr /
             f"{opts[0].num_attackers}_attackers"
-        ),
+        )
 
+    # Remove options that aren't applicable for speed
+    filtered_opts = list()
+    for opt in opts:
+        # Only run OTC with route leaks
+        if opt.ScenarioCls != AccidentalRouteLeak and "OTC" in opt.AdoptPolicyCls.name:
+            continue
+        # EdgeFilter isn't applicable when attacker isn't at the edge
+        elif opt.attacker_subcategory_attr in (
+            ASGroups.TRANSIT.value, ASGroups.ETC.value, ASGroups.INPUT_CLIQUE.value
+        ) and "Edge" in opt.AdoptPolicyCls.name:
+            continue
+        # Pathend and BGPSec variants do nothing against route leaks
+        elif opt.ScenarioCls == AccidentalRouteLeak and (
+            "Path-End" in opt.AdoptPolicyCls.name
+            or "BGPSec" in opt.AdoptPolicyCls.name
+            or "BGP-iSec" in opt.AdoptPolicyCls.name
+        ):
+            continue
+        else:
+            filtered_opts.append(opt)
+
+    # Create sim and run it
     sim = Simulation(
         scenario_configs=tuple(
             [
@@ -40,7 +63,7 @@ def run_sim(opts: list[Opt], output_dir: Path | None = None) -> None:
                     hardcoded_asn_cls_dict=opt.hardcoded_asn_cls_dict,
                     num_attackers=opt.num_attackers
                 )
-                for opt in opts
+                for opt in filtered_opts
             ]
         ),
         output_dir=output_dir,
@@ -48,15 +71,21 @@ def run_sim(opts: list[Opt], output_dir: Path | None = None) -> None:
     )
     sim.run(**run_kwargs)  # type: ignore
 
-standard_hijack_classes = [
-    ROVEdgeFilter,
+standard_classes = [
     PathEnd,
     BGPSec,
     ROV,
     ASPA,
+    BGPiSec,
+    # Edge
     BGPSecEdge,
     PathEndEdge,
+    ROVEdgeFilter,
     ASPAEdge,
+    # OTC
+    OnlyToCustomers,
+    OTCEdge,
+    ASPAOTCEdge,
 ]
 
 #############################
@@ -110,5 +139,32 @@ run_shortest_path_hijack_etc_sim = partial(
     opts=[
         replace(opt, attacker_subcategory_attr=ASGroups.ETC.value)
         for opt in shortest_path_hijack_opts
+    ]
+)
+
+############################
+# AccidentalRouteLeak Sims #
+############################
+
+route_leak_opts = [
+    Opt(
+        AdoptPolicyCls=AdoptPolicyCls,
+        ScenarioCls=AccidentalRouteLeak,
+    ) for AdoptPolicyCls in standard_hijack_classes
+]
+
+run_route_leak_edge_sim = partial(
+    run_sim,
+    opts=[
+        replace(opt, attacker_subcategory_attr=ASGroups.MULTIHOMED.value)
+        for opt in route_leak_opts
+    ],
+)
+
+run_route_leak_transit_sim = partial(
+    run_sim,
+    opts=[
+        replace(opt, attacker_subcategory_attr=ASGroups.TRANSIT.value)
+        for opt in route_leak_opts
     ]
 )
