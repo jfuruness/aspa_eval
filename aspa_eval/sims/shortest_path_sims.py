daeb0e42364ed166d3477e6339ab2e8ac84047d9
Justin Furuness <jfuruness@gmail.com>
2024-09-16 04:24:51 -0400

    Fixed provider cones for bgp-isec [skip ci]

--- aspa_eval/sims/shortest_path_sims.py
+++ aspa_eval/sims/shortest_path_sims.py
@@ -1,10 +1,8 @@
 from dataclasses import replace
 
-from bgpy.shared.constants import SINGLE_DAY_CACHE_DIR
 from bgpy.shared.enums import ASGroups
 from bgpy.simulation_engine import BGP
 from bgpy.simulation_framework import ScenarioConfig, ShortestPathPrefixHijack
-from frozendict import frozendict
 
 from aspa_eval.scenarios import (
     ShortestPathCustomerConeHijack,
@@ -70,27 +68,6 @@ shortest_path_etc_cc_sim = ASPASim(
             attacker_subcategory_attr=ASGroups.ETC.value,
             scenario_label="Doomed ASes",
         ),
-    ),
-    as_graph_constructor_kwargs=frozendict(
-        {
-            "as_graph_collector_kwargs": frozendict(
-                {
-                    "cache_dir": SINGLE_DAY_CACHE_DIR,
-                }
-            ),
-            "as_graph_kwargs": frozendict(
-                {
-                    # When no ASNs are stored, .9gb/core
-                    # When one set of cones is stored, 1.6gb/core
-                    # When both sets of cones are stored, 2.3gb/core
-                    "store_customer_cone_size": True,
-                    "store_customer_cone_asns": True,
-                    "store_provider_cone_size": False,
-                    "store_provider_cone_asns": False,
-                }
-            ),
-            "tsv_path": None,  # Path.home() / "Desktop" / "caida.tsv",
-        }
     ),
     sim_name="ShortestPathPrefixHijack_etc_cc",
 )
--- aspa_eval/sims/utils/aspa_sim.py
+++ aspa_eval/sims/utils/aspa_sim.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 from typing import TYPE_CHECKING
 
+from frozendict import frozendict
+
+from bgpy.shared.constants import SINGLE_DAY_CACHE_DIR
 from bgpy.shared.constants import DIRS
 from bgpy.shared.enums import ASGroups
 from bgpy.simulation_framework import AccidentalRouteLeak, ScenarioConfig, Simulation
@@ -14,6 +17,29 @@ class ASPASim(Simulation):
 
     def __init__(self, *args, **kwargs) -> None:
         kwargs["python_hash_seed"] = 0
+        kwargs["as_graph_constructor_kwargs"] = frozendict(
+            {
+                "as_graph_collector_kwargs": frozendict(
+                    {
+                        "cache_dir": SINGLE_DAY_CACHE_DIR,
+                    }
+                ),
+                "as_graph_kwargs": frozendict(
+                    {
+                        # When no ASNs are stored, .9gb/core
+                        # When one set of cones is stored, 1.6gb/core
+                        # When both sets of cones are stored, 2.3gb/core
+                        # Unfortunately, we need both, since we use BGP-iSec
+                        # and also use customer cones for ASPAwN
+                        "store_customer_cone_size": True,
+                        "store_customer_cone_asns": True,
+                        "store_provider_cone_size": True,
+                        "store_provider_cone_asns": True,
+                    }
+                ),
+                "tsv_path": None,  # Path.home() / "Desktop" / "caida.tsv",
+            }
+        ),
         super().__init__(*args, **kwargs)
 
     @property
