9386617de9a27a07463b5420ba93a7b106029d5c
Justin Furuness <jfuruness@gmail.com>
2024-09-16 05:07:09 -0400

    added BGPSecEdge to ShortestPath attacker [skip ci]

--- aspa_eval/scenarios/paper_shortest_path_prefix_hijack.py
+++ aspa_eval/scenarios/paper_shortest_path_prefix_hijack.py
@@ -2,13 +2,18 @@ from typing import TYPE_CHECKING
 
 from bgpy.simulation_framework import ShortestPathPrefixHijack
 
-from aspa_eval.policies import ASPAEdge, ASPAOTCEdge, PathEndEdge
+from aspa_eval.policies import ASPAEdge, ASPAOTCEdge, PathEndEdge, BGPSecEdge
 
 if TYPE_CHECKING:
     from bgpy.simulation_engine import Policy
 
 
 class PaperShortestPathPrefixHijack(ShortestPathPrefixHijack):
+    @property
+    def rov_policy_classes(self) -> frozenset[type["Policy"]]:
+        """These are policy classes that are susceptible to forged-origin attacks"""
+        return super().rov_policy_classes | frozenset({BGPSecEdge})
+
     @property
     def pathend_policy_classes(self) -> frozenset[type["Policy"]]:
         """Returns policy classes susceptible to PathEnd attacks"""
