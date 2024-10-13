f3daa15c9ac1e563b42f88c6d419473824cf57f9
Ubuntu <ubuntu@ip-172-31-61-50.ec2.internal>
2024-10-13 13:55:48 +0000

    Added timing

--- aspa_eval/__main__.py
+++ aspa_eval/__main__.py
@@ -21,7 +21,7 @@ def main():
 
 if __name__ == "__main__":
     print("47m for 100 trials using 100 CPUs")
-    print("470?m for 100 trials using 100 CPUs")
+    print("278m (~4.6hrs) for 100 trials using 100 CPUs")
     start = time.perf_counter()
     main()
     print(f"{time.perf_counter() - start}s for all sims")  # noqa: T201
