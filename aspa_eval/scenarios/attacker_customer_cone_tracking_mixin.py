7cf070b2d764a4d3be120916e119b8509115eca3
Ubuntu <ubuntu@ip-172-31-61-50.ec2.internal>
2024-10-14 10:18:35 +0000

    fixed syntax bug for customer cones tracking

--- aspa_eval/scenarios/attacker_customer_cone_tracking_mixin.py
+++ aspa_eval/scenarios/attacker_customer_cone_tracking_mixin.py
@@ -33,9 +33,9 @@ class AttackerCustomerConeTrackingMixin(Scenario):
 
         self._attacker_customer_cone_asns: set[int] = set()
         for attacker_asn in self.attacker_asns:
-            assert isinstance(engine[attacker_asn].customer_cone_asns, frozenset), "setting!"
+            assert isinstance(engine.as_graph.as_dict[attacker_asn].customer_cone_asns, frozenset), "setting!"
             self._attacker_customer_cone_asns.update(
-                engine[attacker_asn].customer_cone_asns
+                engine.as_graph.as_dict[attacker_asn].customer_cone_asns
             )
 
         # These are the ASNs we want to avoid tracking
